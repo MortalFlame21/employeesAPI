8b075d3fea65a64f5e6a522334c505de631a7f10
MortalFlame21 <markgio2100@gmail.com>
2024-11-28 01:03:11 +1100

    init express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("swag");
+import express, {
+  NextFunction,
+  ErrorRequestHandler,
+  Request,
+  Response,
+} from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the EmployeesApi!");
+});
+
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    res.status(500).send("Uh oh! An unexpected error occured. :(");
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on port, ${PORT}:\n\t  http://localhost:5000/`
+  );
+});
