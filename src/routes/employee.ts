025bd18290707e50cfc3186a8228f79ee4f71abd
MortalFlame21 <markgio2100@gmail.com>
2024-12-17 11:15:12 +1100

    Edit Employees. Reorganised routes.

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -5,24 +5,6 @@ import { jsonParseBigInt } from "../utils/jsonUtils.js";
 const router = express.Router();
 const prisma = new PrismaClient();
 
-// get employee by id
-router.get("/:id", async (req, res) => {
-  const employee = await prisma.employee.findFirst({
-    where: { id: parseInt(req.params.id) },
-  });
-  const parsedEmployee = jsonParseBigInt(employee);
-  res.json(parsedEmployee);
-});
-
-// get employee by first name
-router.get("/firstName/:name", async (req, res) => {
-  const employee = await prisma.employee.findFirst({
-    where: { first_name: req.params.name },
-  });
-  const parsedEmployee = jsonParseBigInt(employee);
-  res.send(parsedEmployee);
-});
-
 // get all employees
 router.get("/", (req, res) => {
   res.send("get all employees");
@@ -33,6 +15,25 @@ router.post("/", (req, res) => {
   res.send("create employee");
 });
 
+// get normal employees, non-managers
+router.get("/normal", async (req, res) => {
+  // const titles = await prisma.title.findMany({
+  //   distinct: ["title"],
+  //   select: { title: true },
+  // });
+  // const parsedTitle = jsonParseBigInt(titles);
+  // res.send(parsedTitle);
+});
+
+// get employee by id
+router.get("/:id", async (req, res) => {
+  const employee = await prisma.employee.findFirst({
+    where: { id: parseInt(req.params.id) },
+  });
+  const parsedEmployee = jsonParseBigInt(employee);
+  res.json(parsedEmployee);
+});
+
 // update employee
 router.put("/:id", (req, res) => {
   res.send("update employee");
@@ -41,6 +42,15 @@ router.put("/:id", (req, res) => {
 // delete employee
 router.delete("/:id", (req, res) => {
   res.send("delete employee");
+});
+
+// get employee by first name
+router.get("/firstName/:name", async (req, res) => {
+  const employee = await prisma.employee.findFirst({
+    where: { first_name: req.params.name },
+  });
+  const parsedEmployee = jsonParseBigInt(employee);
+  res.send(parsedEmployee);
 });
 
 // get salary of employee by id
@@ -58,18 +68,17 @@ router.put("/:id/salary", (req, res) => {
 });
 
 // get title of employee by id
-router.get("/:id/title", (req, res) => {
-  res.send("get title of employee by id");
+router.get("/:id/title", async (req, res) => {
+  const employeeTitle = await prisma.title.findFirst({
+    where: { employee_id: parseInt(req.params.id) },
+  });
+  const parsedTitle = jsonParseBigInt(employeeTitle);
+  res.json(parsedTitle);
 });
 
 // update title
 router.put("/:id/title", (req, res) => {
   res.send("update title");
-});
-
-// get normal employees
-router.get("/normal", (req, res) => {
-  res.send("get normal employees");
 });
 
 export default router;
