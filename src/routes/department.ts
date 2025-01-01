5a23fa4ede1a06fdc5046d7d0006414f4f7f680e
MortalFlame21 <markgio2100@gmail.com>
2025-01-01 22:34:10 +1100

    add PUT department

--- src/routes/department.ts
+++ src/routes/department.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
 // get employee by department
 router.get("/:department", (req, res) => {
@@ -12,9 +14,28 @@ router.put("/department", (req, res) => {
   res.send("update employee department");
 });
 
-// add a department
-router.post("/", (req, res) => {
-  res.send("add a department");
+// add department
+router.put("/", async (req, res) => {
+  const { deptName } = req.body;
+
+  const newDepartment = await prisma.department.findFirst({
+    orderBy: {
+      id: "desc",
+    },
+  });
+
+  const idNum = parseInt(newDepartment!.id.replace("d", "")) + 1;
+
+  prisma.department.create({
+    data: {
+      id: `d${String(idNum).padStart(3, "0")}`,
+      dept_name: deptName,
+    },
+  });
+
+  res.send({
+    new_department: newDepartment,
+  });
 });
 
 // delete a department, consequently updating all employees in that department
