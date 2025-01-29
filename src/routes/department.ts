e3b81e035504815d45de8cb602420579d5ffa92d
MortalFlame21 <markgio2100@gmail.com>
2025-01-29 22:55:06 +1100

    Edit GET, GET /:dept_id, PUT for departments.

--- src/routes/department.ts
+++ src/routes/department.ts
@@ -1,44 +1,65 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { jsonParseBigInt } from "@/utils/jsonUtils.js";
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
-// get employee by department
-router.get("/:department", (req, res) => {
-  res.send("get employee by department");
+router.get("/", async (req, res) => {
+  const departments = await prisma.department.findMany({
+    take: 10,
+    skip: 1,
+  });
+  res.send(departments);
 });
 
-// update employee department
-router.put("/department", (req, res) => {
-  res.send("update employee department");
+// get employee by department
+// probs put in employees
+// remember id can only be char(4)
+router.get("/:department_id", async (req, res) => {
+  // if i want to use "joins"
+  // https://www.prisma.io/blog/prisma-orm-now-lets-you-choose-the-best-join-strategy-preview
+  const department_id = req.params.department_id;
+  console.log(department_id);
+  const employees = await prisma.department_employee.findMany({
+    where: { department_id: department_id },
+    take: 10,
+    skip: 1,
+  });
+  const employees_ = await prisma.employee.findMany({
+    where: {
+      id: {
+        in: employees.map(({ employee_id }) => {
+          return employee_id;
+        }),
+      },
+    },
+  });
+  res.json({ employees: jsonParseBigInt(employees_) });
 });
 
 // add department
 router.put("/", async (req, res) => {
-  const { deptName } = req.body;
+  const { department_name } = req.body;
 
-  const newDepartment = await prisma.department.findFirst({
-    orderBy: {
-      id: "desc",
-    },
+  const latestDepartment = await prisma.department.findFirst({
+    orderBy: { id: "desc" },
   });
 
-  const idNum = parseInt(newDepartment!.id.replace("d", "")) + 1;
+  const idNum = parseInt(latestDepartment!.id.replace("d", "")) + 1;
 
-  prisma.department.create({
+  const newDepartment = await prisma.department.create({
     data: {
       id: `d${String(idNum).padStart(3, "0")}`,
-      dept_name: deptName,
+      dept_name: department_name,
     },
   });
 
-  res.send({
-    new_department: newDepartment,
-  });
+  res.send({ new_department: newDepartment });
 });
 
-// delete a department, consequently updating all employees in that department
+// delete a department
+// do later: consequently updating all employees in that department
 router.delete("/:department", (req, res) => {
   res.send("delete a department");
 });
