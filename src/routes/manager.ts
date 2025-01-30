fa21349cac2591d1fee30737c5ed12f634370469
MortalFlame21 <markgio2100@gmail.com>
2025-01-30 23:07:46 +1100

    Add PUT managers. Just like employee title, salary etc.

--- src/routes/manager.ts
+++ src/routes/manager.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, type department_manager } from "@prisma/client";
 import { jsonParseBigInt } from "@/utils/jsonUtils.js";
 
 const router = express.Router();
@@ -15,8 +15,45 @@ router.get("/", async (req, res) => {
 });
 
 // create manager
-router.post("/", (req, res) => {
-  res.send("create manager");
+router.put("/", async (req, res) => {
+  const { employee_id, department_id, from_date, to_date } = req.body;
+
+  const data: department_manager = {
+    employee_id: BigInt(employee_id),
+    department_id: department_id,
+    from_date: new Date(from_date),
+    to_date: new Date(to_date),
+  };
+
+  const where = {
+    employee_id: data.employee_id,
+    department_id: data.department_id,
+  };
+
+  const oldManager = await prisma.department_manager.findFirst({
+    where: where,
+    orderBy: { from_date: "desc" },
+  });
+
+  if (!oldManager)
+    throw `employee_id: ${data.employee_id},\n
+    department_id: ${data.department_id}\n
+    does not exist in Department table. Nothing to update.`;
+
+  await prisma.department_manager.update({
+    where: {
+      employee_id_department_id: where,
+      from_date: oldManager.from_date,
+    },
+    data: { to_date: from_date },
+  });
+
+  const newManager = await prisma.department_manager.create({ data: data });
+
+  res.json({
+    old_manager: jsonParseBigInt(oldManager),
+    new_manager: jsonParseBigInt(newManager),
+  });
 });
 
 // delete manager
