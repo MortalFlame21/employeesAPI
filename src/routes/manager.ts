63f71d2a18c528e9e8ee6ed0a04f3348fbb3afd1
MortalFlame21 <markgio2100@gmail.com>
2025-01-30 23:35:52 +1100

    Add POST managers. Also fix PUT managers. Both working swag.

--- src/routes/manager.ts
+++ src/routes/manager.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { PrismaClient, type department_manager } from "@prisma/client";
 import { jsonParseBigInt } from "@/utils/jsonUtils.js";
+import { request } from "http";
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -25,27 +26,24 @@ router.put("/", async (req, res) => {
     to_date: new Date(to_date),
   };
 
-  const where = {
-    employee_id: data.employee_id,
-    department_id: data.department_id,
-  };
-
   const oldManager = await prisma.department_manager.findFirst({
-    where: where,
+    where: { employee_id: data.employee_id },
     orderBy: { from_date: "desc" },
   });
 
   if (!oldManager)
-    throw `employee_id: ${data.employee_id},\n
-    department_id: ${data.department_id}\n
+    throw `employee_id: ${data.employee_id}
     does not exist in Department table. Nothing to update.`;
 
   await prisma.department_manager.update({
     where: {
-      employee_id_department_id: where,
+      employee_id_department_id: {
+        employee_id: data.employee_id,
+        department_id: oldManager.department_id,
+      },
       from_date: oldManager.from_date,
     },
-    data: { to_date: from_date },
+    data: { to_date: data.from_date },
   });
 
   const newManager = await prisma.department_manager.create({ data: data });
@@ -54,6 +52,21 @@ router.put("/", async (req, res) => {
     old_manager: jsonParseBigInt(oldManager),
     new_manager: jsonParseBigInt(newManager),
   });
+});
+
+router.post("/", async (req, res) => {
+  const { employee_id, department_id, from_date, to_date } = req.body;
+
+  const newManager = await prisma.department_manager.create({
+    data: {
+      employee_id: BigInt(employee_id),
+      department_id: department_id,
+      from_date: new Date(from_date),
+      to_date: new Date(to_date),
+    },
+  });
+
+  res.send({ new_manager: jsonParseBigInt(newManager) });
 });
 
 // delete manager
