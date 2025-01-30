b0337732e14219a4ba00aa6f1daba31c83ebe1cf
MortalFlame21 <markgio2100@gmail.com>
2025-01-30 22:27:25 +1100

    Complete GET for manager route

--- src/routes/manager.ts
+++ src/routes/manager.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { jsonParseBigInt } from "@/utils/jsonUtils.js";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
 // get managers
-router.get("/", (req, res) => {
-  res.send("get managers");
+router.get("/", async (req, res) => {
+  const managers = await prisma.department_manager.findMany({
+    take: 10,
+    skip: 1,
+  });
+  res.send(jsonParseBigInt(managers));
 });
 
 // create manager
