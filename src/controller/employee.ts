19dbe4186cfa8e50b9b590d6abae0101d1781f4b
MortalFlame21 <markgio2100@gmail.com>
2025-02-03 23:10:31 +1100

    Reorganised Controller. Order of Singular, Many, Filter/Updates

--- src/controller/employee.ts
+++ src/controller/employee.ts
@@ -9,27 +9,6 @@ import { jsonParseBigInt } from "../utils/jsonUtils.js";
 const prisma = new PrismaClient();
 
 const EmployeeController = {
-  getEmployees: async (req: Request, res: Response) => {
-    let offset = parseInt(req.query.offset as string) || 1;
-    let limit = parseInt(req.query.limit as string) || 10;
-    if (limit > 100) limit = 100;
-
-    const end = offset * limit;
-    const nextPage = `${req.baseUrl}/?offset=${end}&limit=${limit}`;
-
-    const employees = await prisma.employee.findMany({
-      skip: offset,
-      take: limit,
-    });
-
-    res.json({
-      offset: offset,
-      limit: limit,
-      nextPage: nextPage,
-      results: jsonParseBigInt(employees),
-    });
-  },
-
   createEmployee: async (req: Request, res: Response) => {
     const { id, birth_date, first_name, last_name, gender, hire_date } =
       req.body;
@@ -55,6 +34,105 @@ const EmployeeController = {
       },
     });
     res.json({ deletedUser: jsonParseBigInt(employee) });
+  },
+
+  findByFirstName: async (req: Request, res: Response) => {
+    const employee = await prisma.employee.findFirst({
+      where: {
+        first_name: {
+          equals: req.params.name,
+          mode: "insensitive",
+        },
+      },
+    });
+    res.json(jsonParseBigInt(employee));
+  },
+
+  getEmployees: async (req: Request, res: Response) => {
+    let offset = parseInt(req.query.offset as string) || 1;
+    let limit = parseInt(req.query.limit as string) || 10;
+    if (limit > 100) limit = 100;
+
+    const end = offset * limit;
+    const nextPage = `${req.baseUrl}/?offset=${end}&limit=${limit}`;
+
+    const employees = await prisma.employee.findMany({
+      skip: offset,
+      take: limit,
+    });
+
+    res.json({
+      offset: offset,
+      limit: limit,
+      nextPage: nextPage,
+      results: jsonParseBigInt(employees),
+    });
+  },
+
+  findByTitle: async (req: Request, res: Response) => {
+    const employees = await prisma.title.findMany({
+      where: {
+        title: {
+          equals: req.params.title,
+          mode: "insensitive",
+        },
+      },
+      take: 10,
+      skip: 1,
+    });
+    const employees_ = await prisma.employee.findMany({
+      where: {
+        id: {
+          in: employees.map(({ employee_id }) => {
+            return employee_id;
+          }),
+        },
+      },
+    });
+    res.send({ employees: jsonParseBigInt(employees_) });
+  },
+
+  findBySalary: async (req: Request, res: Response) => {
+    const min_salary = BigInt((req.query.min_salary as string) || 1);
+    const max_salary = BigInt((req.query.max_salary as string) || 999999);
+    const employees = await prisma.salary.findMany({
+      where: {
+        amount: {
+          gte: min_salary,
+          lte: max_salary,
+        },
+      },
+      take: 10,
+      skip: 0,
+      distinct: ["employee_id"],
+    });
+    const employees_ = await prisma.employee.findMany({
+      where: {
+        id: {
+          in: employees.map(({ employee_id }) => {
+            return employee_id;
+          }),
+        },
+      },
+    });
+    res.send({ employees: jsonParseBigInt(employees_) });
+  },
+
+  findByHireDate: async (req: Request, res: Response) => {
+    const start_hire_date = new Date(req.query.start_hire_date as string);
+    const end_hire_date = new Date(req.query.end_hire_date as string);
+    const employees = await prisma.employee.findMany({
+      where: {
+        hire_date: {
+          gte: start_hire_date,
+          lte: end_hire_date,
+        },
+      },
+      take: 10,
+      skip: 0,
+      distinct: ["id"],
+    });
+    res.send({ employees: jsonParseBigInt(employees) });
   },
 
   upsertSalary: async (req: Request, res: Response) => {
@@ -215,84 +293,6 @@ const EmployeeController = {
     });
 
     res.json({ new_employee_department: jsonParseBigInt(newDepartment) });
-  },
-
-  findByFirstName: async (req: Request, res: Response) => {
-    const employee = await prisma.employee.findFirst({
-      where: {
-        first_name: {
-          equals: req.params.name,
-          mode: "insensitive",
-        },
-      },
-    });
-    res.json(jsonParseBigInt(employee));
-  },
-
-  findByTitle: async (req: Request, res: Response) => {
-    const employees = await prisma.title.findMany({
-      where: {
-        title: {
-          equals: req.params.title,
-          mode: "insensitive",
-        },
-      },
-      take: 10,
-      skip: 1,
-    });
-    const employees_ = await prisma.employee.findMany({
-      where: {
-        id: {
-          in: employees.map(({ employee_id }) => {
-            return employee_id;
-          }),
-        },
-      },
-    });
-    res.send({ employees: jsonParseBigInt(employees_) });
-  },
-
-  findBySalary: async (req: Request, res: Response) => {
-    const min_salary = BigInt((req.query.min_salary as string) || 1);
-    const max_salary = BigInt((req.query.max_salary as string) || 999999);
-    const employees = await prisma.salary.findMany({
-      where: {
-        amount: {
-          gte: min_salary,
-          lte: max_salary,
-        },
-      },
-      take: 10,
-      skip: 0,
-      distinct: ["employee_id"],
-    });
-    const employees_ = await prisma.employee.findMany({
-      where: {
-        id: {
-          in: employees.map(({ employee_id }) => {
-            return employee_id;
-          }),
-        },
-      },
-    });
-    res.send({ employees: jsonParseBigInt(employees_) });
-  },
-
-  findByHireDate: async (req: Request, res: Response) => {
-    const start_hire_date = new Date(req.query.start_hire_date as string);
-    const end_hire_date = new Date(req.query.end_hire_date as string);
-    const employees = await prisma.employee.findMany({
-      where: {
-        hire_date: {
-          gte: start_hire_date,
-          lte: end_hire_date,
-        },
-      },
-      take: 10,
-      skip: 0,
-      distinct: ["id"],
-    });
-    res.send({ employees: jsonParseBigInt(employees) });
   },
 };
 
