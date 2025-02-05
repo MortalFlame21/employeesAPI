54857cdc4052cfba78538ffd27e783af3de0ac20
MortalFlame21 <markgio2100@gmail.com>
2025-02-06 10:29:08 +1100

    add salary model as z.obj. idk if i would use it

--- src/schema/schema.prisma.ts
+++ src/schema/schema.prisma.ts
@@ -5,10 +5,17 @@ import { z } from "zod";
 // **
 
 export const z_employeeSchema = z.object({
-  id: z.coerce.bigint(),
+  id: z.coerce.bigint().nonnegative(),
   birth_date: z.date(),
-  first_name: z.string(),
-  last_name: z.string(),
+  first_name: z.string().min(1).trim(),
+  last_name: z.string().min(1).trim(),
   gender: z.enum(["M", "F"]),
   hire_date: z.date(),
+});
+
+export const z_salarySchema = z.object({
+  employee_id: z.coerce.bigint().nonnegative(),
+  amount: z.coerce.bigint(),
+  from_date: z.date(),
+  to_date: z.date(),
 });
