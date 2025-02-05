2679e923c1edc8b8c1529be03551c0c0f760b389
MortalFlame21 <markgio2100@gmail.com>
2025-02-06 10:57:28 +1100

    Edit schema.prisma.ts. Better date type.

--- src/schema/schema.prisma.ts
+++ src/schema/schema.prisma.ts
@@ -4,13 +4,24 @@ import { z } from "zod";
 // Prisma models translated into zod schemas/objects
 // **
 
+const z_dateMessage = "Please enter a valid date! See: \
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#date_time_string_format for more information" as const;
+
+const z_date = z.coerce.date({
+  // https://github.com/colinhacks/zod/discussions/1851
+  errorMap: ({ code }, { defaultError }) => {
+    if (code == "invalid_date") return { message: z_dateMessage };
+    return { message: defaultError };
+  },
+});
+
 export const z_employeeSchema = z.object({
   id: z.coerce.bigint().nonnegative(),
-  birth_date: z.date(),
+  birth_date: z_date.max(new Date()),
   first_name: z.string().min(1).trim(),
   last_name: z.string().min(1).trim(),
   gender: z.enum(["M", "F"]),
-  hire_date: z.date(),
+  hire_date: z_date.max(new Date()),
 });
 
 export const z_salarySchema = z.object({
