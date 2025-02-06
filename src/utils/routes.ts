073f197dba18a5283dd9245deaad8d9b900ae9c4
MortalFlame21 <markgio2100@gmail.com>
2025-02-06 11:27:06 +1100

    move routes.ts to utils. More of a helper thing now.

---
+++ src/utils/routes.ts
@@ -0,0 +1,27 @@
+import type { Request } from "express";
+import { z } from "zod";
+
+// **
+// Some helper for route validation
+// **
+
+// ** helper **
+// would like not to rely of Request
+export function paginationPageOffset(req: Request) {
+  const offset = parseInt(req.query.offset as string) ?? 0;
+  const limit = parseInt(req.query.limit as string) ?? 10;
+  const end = offset * limit;
+
+  return {
+    offset: offset,
+    limit: limit,
+    end: end,
+    nextPage: `${req.baseUrl}/?offset=${end}&limit=${limit}`,
+  };
+}
+
+// ** zod types **
+export const z_pageOffset = z.object({
+  offset: z.coerce.number().gte(0).int().safe().optional().default(0),
+  limit: z.coerce.number().gte(0).int().safe().optional().default(10),
+});
