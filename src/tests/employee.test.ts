176594b8a3b0f35bb3b73c104788f1b51f478c18
MortalFlame21 <markgio2100@gmail.com>
2025-03-14 12:01:45 +1100

    Edit employees.test.ts. Add more GET tests.

--- src/tests/employee.test.ts
+++ src/tests/employee.test.ts
@@ -15,15 +15,15 @@ describe(`${url}`, () => {
       expect(res.body.results).toHaveLength(100);
     });
 
-    test("employee id 100", async () => {
+    test("employee id 100 (not-existing)", async () => {
       const res = await req
         .get(`${url}/100`)
         .expect("Content-Type", /json/)
         .expect(200);
-      expect(res.body).toBe(null);
+      expect(res.body).toBeNull();
     });
 
-    test("employee name JaqueS (existing)", async () => {
+    test("employee name JaqueS", async () => {
       const res = await req
         .get(`${url}/firstName/JaqueS`)
         .expect("Content-Type", /json/)
@@ -31,13 +31,37 @@ describe(`${url}`, () => {
       expect(res.body.id).toBe("10152");
     });
 
-    test.todo("employee salary by id", () => {});
+    test("employee salary by id 100 (not-existing)", async () => {
+      const res = await req
+        .get(`${url}/100/salary`)
+        .expect("Content-Type", /json/)
+        .expect(200);
+      expect(res.body).toBeNull();
+    });
 
-    test.todo("employee title by id", () => {});
+    test("employee title by id 53599", async () => {
+      const res = await req
+        .get(`${url}/53599/title`)
+        .expect("Content-Type", /json/)
+        .expect(200);
+      expect(res.body.employee_id).toBe("53599");
+    });
 
-    test.todo("employees with title", () => {});
+    test("employees with title Senior Engineer", async () => {
+      const res = await req
+        .get(`${url}/title/Senior Engineer`)
+        .expect("Content-Type", /json/)
+        .expect(200);
+      expect(res.body.results.length).toBeGreaterThanOrEqual(0);
+    });
 
-    test.todo("employee within salary range", () => {});
+    test("first 5 employees having 155000 <= salary <= 999999", async () => {
+      const res = await req
+        .get(`${url}/salary?&limit=5&min_salary=155000&max_salary=999999`)
+        .expect("Content-Type", /json/)
+        .expect(200);
+      expect(res.body.employees).toHaveLength(5);
+    });
 
     test.todo("employee within hire date", () => {});
 
