cc43b8f37d632b85c05a8f305264bc29fc2aaa96
MortalFlame21 <markgio2100@gmail.com>
2025-06-07 23:35:42 +1000

    Fix manager.ts. insertManager is actual behavior.

--- src/controller/manager.ts
+++ src/controller/manager.ts
@@ -60,7 +60,6 @@ const ManagerController = {
       const newManager = await prisma.department_manager.create({ data: data });
 
       res.json({
-        old_manager: jsonParseBigInt(oldManager),
         new_manager: jsonParseBigInt(newManager),
       });
     } catch (e) {
@@ -72,37 +71,17 @@ const ManagerController = {
     try {
       const { employee_id, department_id, from_date, to_date } = req.body;
 
-      const data: department_manager = {
-        employee_id: BigInt(employee_id),
-        department_id: department_id,
-        from_date: new Date(from_date),
-        to_date: new Date(to_date),
-      };
-
-      const oldManager = await prisma.department_manager.findFirst({
-        where: { employee_id: data.employee_id },
-        orderBy: { from_date: "desc" },
-      });
-
-      if (!oldManager)
-        throw `employee_id: ${data.employee_id} \
-does not exist in Manager table. Nothing to update.`;
-
-      await prisma.department_manager.update({
-        where: {
-          employee_id_department_id: {
-            employee_id: data.employee_id,
-            department_id: oldManager.department_id,
-          },
-          from_date: oldManager.from_date,
+      const newManager = await prisma.department_manager.create({
+        data: {
+          employee_id: BigInt(employee_id),
+          department_id: department_id,
+          from_date: new Date(from_date),
+          to_date: new Date(to_date),
         },
-        data: { to_date: data.from_date },
       });
 
-      const newManager = await prisma.department_manager.create({ data: data });
-
+      console.log("here");
       res.json({
-        old_manager: jsonParseBigInt(oldManager),
         new_manager: jsonParseBigInt(newManager),
       });
     } catch (e) {
