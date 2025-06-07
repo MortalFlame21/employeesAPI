bb492ab78dad5dcd8fce57e3038bccec9f3f52b6
MortalFlame21 <markgio2100@gmail.com>
2025-06-07 23:02:41 +1000

    Edit manager.test.ts. Done with POST testing.

--- src/tests/manager.test.ts
+++ src/tests/manager.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { afterAll, describe, expect, test } from "vitest";
 import supertest from "supertest";
 import app from "@/app.js";
 
@@ -19,14 +19,69 @@ describe("Manager", () => {
   });
 
   describe("POST", () => {
-    test("Change manager to _department", () => {});
+    const bodies = [
+      {
+        employee_id: 500097,
+        from_date: "2024-05-13",
+        to_date: "9999-01-01",
+        department_id: "d017",
+      },
+      {
+        employee_id: 500088,
+        from_date: "2025-03-10",
+        to_date: "9999-01-01",
+        department_id: "d019",
+      },
+    ] as const;
+
+    afterAll(async () => {
+      try {
+        await req
+          .delete(`${url}`)
+          .send({ ...bodies[0], department_id: "d015" });
+        await req.delete(`${url}`).send(bodies[1]);
+      } catch (e) {
+        console.log("POST employee: afterAll: Error caught.");
+        console.log(e);
+      }
+    });
+
+    test("Change manager to _department", async () => {
+      const res = await req
+        .post(`${url}`)
+        .send(bodies[0])
+        .expect("Content-type", /json/)
+        .expect(200);
+      expect(res.body.old_manager).not.null;
+      expect(res.body.new_manager.employee_id).equal(
+        bodies[0].employee_id.toString()
+      );
+    });
+
+    test("Change non-manager _Daquavious Pork to _Instagram Reels", async () => {
+      const res = await req
+        .post(`${url}`)
+        .send(bodies[1])
+        .expect("Content-type", /json/)
+        .expect(400);
+      console.log(res.body);
+      // UnknownError atm because I don't utilise AggreggateError
+      expect(res.body.error_type).toContain("UnknownError");
+    });
   });
 
-  describe("PUT", () => {
-    test("Add new manager to _department", () => {});
+  describe.todo("PUT", () => {
+    afterAll(() => {
+      // delete
+    });
+    test.todo("Add new manager to _department", async () => {});
   });
 
-  describe("DELETE", () => {
-    test("Delete manager from _department", () => {});
+  describe.todo("DELETE", () => {
+    afterAll(() => {
+      // put
+    });
+
+    test.todo("Delete manager from _department", async () => {});
   });
 });
